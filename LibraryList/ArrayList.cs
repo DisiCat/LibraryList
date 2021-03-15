1fa90cd35a7d390f29b669b3ae3cb5e64abf832b
Disic <Nickincorname@gmail.com>
2021-03-15 20:31:04 +0200

    LibraryList
    completed 10 points

---
+++ LibraryList/ArrayList.cs
@@ -0,0 +1,260 @@
+﻿using System;
+
+namespace LibraryList
+{
+    public class ArrayList
+    {
+        public int Lenght { get; private set; }
+
+        private int[] _array;
+
+        public ArrayList()
+        {
+            Lenght = 0;
+            _array = new int[10];
+        }
+
+        public void Add(int value)
+        {
+            if (Lenght == _array.Length) {
+                UpSize();
+            }
+            _array[Lenght] = value;
+            Lenght++;
+        }
+
+        public void AddInStart(int value)
+        {
+            if (Lenght != 0)
+            {
+                if (Lenght == _array.Length)
+                {
+                    UpSize();
+                }
+
+                int[] tempArray = new int[_array.Length + 1];
+                tempArray[0] = value;
+                int j = 0;
+                for (int i = 0; i < _array.Length; i++)
+                {
+                    tempArray[++j] = _array[i];
+                }
+
+                _array = tempArray;
+
+            }
+            else
+            {
+                _array[0] = value;
+            }
+
+            Lenght++;
+        }
+
+        public void Insert(int index, int value)
+        {
+            if (index < Lenght)
+            {
+                if (Lenght == _array.Length)
+                {
+                    UpSize();
+                }
+
+                int[] tempArray = new int[_array.Length + 1];
+                int j = 0;
+                for (int i = 0; i < _array.Length; i++)
+                {
+                    if (i == index)
+                    {
+                        tempArray[j++] = value;
+                    }
+                    tempArray[j++] = _array[i];
+                }
+
+                _array = tempArray;
+                Lenght++;
+            }
+            else
+            {
+
+            throw new IndexOutOfRangeException("Index Out Of Randge ");
+
+            }
+        }
+
+        public int IndexAccess(int index)
+        {
+            if(index < Lenght)
+            {
+                return _array[index];
+            }
+
+            throw new IndexOutOfRangeException("Index Out Of Randge ");
+        }
+
+        public void RemoveLast()
+        {   if (Lenght != 0) { 
+            int newLength = _array.Length - 1;
+            if (Lenght < _array.Length / 2)
+            {
+                newLength = (int)(_array.Length / 1.33d + 1);
+            }
+
+            int[] tempArray = new int[newLength];
+            for (int i = 0; i < tempArray.Length; i++)
+            {
+                tempArray[i] = _array[i];
+            }
+
+            _array = tempArray;
+            Lenght--;
+            } else
+            {
+                throw new Exception("Array pustoi");
+            }
+        }
+
+        public void RemoveFirst()
+        {
+            if (Lenght != 0)
+            {
+                int newLength = _array.Length - 1;
+                if (Lenght < _array.Length / 2)
+                {
+                    newLength = (int)(_array.Length / 1.33d + 1);
+                }
+
+                int[] tempArray = new int[newLength];
+
+                int j = 0;
+                for (int i = 1; i < tempArray.Length; i++)
+                {
+                    tempArray[j++] = _array[i];
+                }
+
+                _array = tempArray;
+                Lenght--;
+            } else
+            {
+                throw new Exception("Array pustoi");
+            }
+        }
+
+        public void RemoveAt(int index)
+        {
+            if (Lenght != 0)
+            {
+                int newLength = _array.Length - 1;
+                if (Lenght < _array.Length / 2)
+                {
+                    newLength = (int)(_array.Length / 1.33d + 1);
+                }
+
+                int[] tempArray = new int[newLength];
+
+                int j = 0;
+                for (int i = 0; i < tempArray.Length; i++)
+                {
+
+                    if (i != index)
+                    {
+                        tempArray[j++] = _array[i];
+                    }
+                }
+
+                _array = tempArray;
+                Lenght--;
+            }
+            else
+            {
+                throw new Exception("Array pustoi");
+            }
+        }
+        
+        public void RemoveNElementsFromEnd(int nElelements)
+        {
+            if (Lenght != 0)
+            {
+                int newLength = Lenght - nElelements;
+                
+                int[] tempArray = new int[newLength];
+                for (int i = 0; i < tempArray.Length; i++)
+                {
+                    tempArray[i] = _array[i];
+                }
+
+                _array = tempArray;
+                Lenght-= nElelements;
+            }
+            else
+            {
+                throw new Exception("Array pustoi");
+            }
+        }
+
+        public void RemoveNElementsAt(int index, int nElelements)
+        {
+            if (Lenght >= (nElelements + index))
+            {
+                int newLength = Lenght - nElelements;
+                int SwapValue = nElelements;
+                int[] tempArray = new int[newLength];
+
+                int j = 0;
+                for (int i = 0; i < tempArray.Length; i++)
+                {
+
+                    if (i != index)
+                    {
+                        tempArray[i] = _array[j++];
+                    }
+                    else
+                    {
+                        j += SwapValue;
+                        tempArray[i] = _array[j++];
+                    }
+                }
+
+                _array = tempArray;
+                Lenght -= nElelements;
+            }
+            else
+            {
+                throw new Exception("Array pustoi");
+            }
+        }
+        public void RemoveNElementsFromStart(int nElelements)
+        {
+            if (Lenght >= nElelements)
+            {
+                int newLength = Lenght - nElelements;
+                int SwapValue = nElelements;
+                int[] tempArray = new int[newLength];
+                for (int i = 0; i < tempArray.Length; i++)
+                {
+                    tempArray[i] = _array[SwapValue++];
+                }
+
+                _array = tempArray;
+                Lenght -= nElelements;
+            }
+            else
+            {
+                throw new Exception("Array pustoi");
+            }
+        }
+        private void UpSize()
+        {
+            int newLength = (int)(_array.Length * 1.33d + 1);
+            int[] tempArray = new int[newLength];
+            for (int i = 0; i < _array.Length; i++)
+            {
+                tempArray[i] = _array[i];
+            }
+
+            _array = tempArray;
+        }
+    }
+}
+   
+
---
+++ ProverkaLibrary/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using LibraryList;
+
+namespace ProverkaLibrary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList array = new ArrayList();
+            //array.Add(1);
+            //Console.WriteLine(array.IndexAccess(0));
+            //array.AddInStart(11);
+            //Console.WriteLine(array.IndexAccess(0));
+            //array.Insert(0, 5);
+            //Console.WriteLine(array.IndexAccess(0));
+            //array.Insert(1, 40);
+            //Console.WriteLine(array.IndexAccess(1));
+            //Console.WriteLine("ВЕсь массив :");
+            //for (int i = 0; i < array.Lenght; i++)
+            //{
+            //    Console.WriteLine(array.IndexAccess(i));
+            //}
+
+            //array.RemoveLast();
+
+            //Console.WriteLine("ВЕсь массив :");
+            //for (int i = 0; i < array.Lenght; i++)
+            //{
+            //    Console.WriteLine(array.IndexAccess(i));
+            //}
+
+            //array.RemoveFirst();
+            //Console.WriteLine("ВЕсь массив :");
+            //for (int i = 0; i < array.Lenght; i++)
+            //{
+            //    Console.WriteLine(array.IndexAccess(i));
+            //}
+            array.Add(1);
+            array.Add(2);
+            array.Add(3);
+            array.Add(4);
+            array.Add(5);
+            array.Add(6);
+            Console.WriteLine("ВЕсь массив :");
+            for (int i = 0; i < array.Lenght; i++)
+            {
+                Console.WriteLine(array.IndexAccess(i));
+            }
+            array.RemoveNElementsAt(1,6);
+            Console.WriteLine("ВЕсь массив :");
+            for (int i = 0; i < array.Lenght; i++)
+            {
+                Console.WriteLine(array.IndexAccess(i));
+            }
+            Console.Write("Че? ");
+        }
+    }
+}
+
